0aaa20941495f550576768458318390e063b3a9f
SetHet <36393893+SetHet@users.noreply.github.com>
2024-06-25 23:10:02 -0400

    Builder Strategy

--- func.py
+++ func.py
@@ -5,4 +5,6 @@ class Function:
     def __init__(self, name):
         self.name = name.replace(" ", "_")
 
-    def addFunction(self, type, value, player = ""): self.functionList.append(f"{type} {player} {value}")
+    def addFunction(self, type, value, player = ""): 
+        self.functionList.append(f"{type} {player} {value}")
+        return self
